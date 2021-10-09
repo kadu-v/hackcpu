bd0c95f6e08a56bdbd9a7190528a7714d8872c06
kadu-v <kazuonsunday@yahoo.co.jp>
2021-10-09 20:01:27 +0900

    Rename: data to out_data

--- src/design/hack_decoder.v
+++ src/design/hack_decoder.v
@@ -1,7 +1,7 @@
 module hack_decoder (
     input wire  [15:0] inst,
 
-    output wire [14:0] out
+    output wire [14:0] out_data
 );
 wire write_m;
 wire load_d;
@@ -14,5 +14,5 @@ assign load_a   = inst[5] & ~inst[15];
 assign sel_mux2 = inst[12] & inst[15];
 
 
-assign out = {inst[0], inst[1], inst[2], write_m, load_a, inst[6], inst[7], inst[8], inst[9], inst[10], inst[11], sel_mux2, inst[15], inst[15]};
+assign out_data = {inst[0], inst[1], inst[2], write_m, load_a, inst[6], inst[7], inst[8], inst[9], inst[10], inst[11], sel_mux2, inst[15], inst[15]};
 endmodule
--- src/design/hack_mux.v
+++ src/design/hack_mux.v
@@ -3,8 +3,8 @@ module hack_mux (
     input   [15:0]  in_data2,
     input           mux_sel,
 
-    output  [15:0]  out
+    output  [15:0]  out_data
 );
 
-assign out = mux_sel ? in_data1 : in_data2;
+assign out_data = mux_sel ? in_data1 : in_data2;
 endmodule
