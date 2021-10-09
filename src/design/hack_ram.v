4a6f8bae88928e98c441bbba692d7641c478179b
kadu-v <kazuonsunday@yahoo.co.jp>
2021-10-09 21:14:47 +0900

    Add: ram and rom

---
+++ src/design/hack_ram.v
@@ -0,0 +1,21 @@
+module hack_ram (
+    input wire          clk,
+    input wire          load,
+    input wire [15:0]   addr,
+    input wire [15:0]   in_data,
+
+    output reg [15:0]   out_data
+);
+reg [15:0] mem [16383:0]; // 16 Kb
+
+// データの書き込み
+// 立ち上がりエッジで書き込む
+always @(posedge clk) begin
+    if(load) begin
+        mem[addr] <= in_data;
+    end
+    out_data <= mem[addr];
+end
+
+
+endmodule
---
+++ src/design/hack_rom.v
@@ -0,0 +1,16 @@
+module hack_rom (
+    input wire          clk,
+    input wire [13:0]   addr,
+
+    output reg [15:0]   out_data
+);
+    reg [15:0] mem [32766:0]; // 32K
+
+    always @(posedge clk) begin
+        out_data <= mem[addr];
+    end
+    
+    initial begin
+        $readmemh("program.hex", mem);
+    end
+endmodule
